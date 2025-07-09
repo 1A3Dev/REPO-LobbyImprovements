d0f716ce6f0260db584c405fc019b3db85e3c9b5
1A3 <mcplayer1a3@gmail.com>
2025-07-09 04:59:43 +0100

    skip splash screen faster

--- source/Patches/FastStartup.cs
+++ source/Patches/FastStartup.cs
@@ -22,7 +22,7 @@ namespace LobbyImprovements.Patches
         [HarmonyWrapSafe]
         private static void RunManager_Awake(RunManager __instance)
         {
-            if (PluginLoader.splashScreenUIEnabled.Value) return;
+            if (PluginLoader.splashScreenUIEnabled.Value || __instance.levelCurrent != __instance.levelSplashScreen) return;
             __instance.levelCurrent = __instance.levelMainMenu;
         }
     }
