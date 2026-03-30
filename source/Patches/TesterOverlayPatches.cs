d5fd5818273835d07deb5079d632aa60256c3bfd
1A3Dev <mcplayer1a3@gmail.com>
2026-03-30 13:21:01 +0100

    Fixed initial max player count

--- source/Patches/TesterOverlayPatches.cs
+++ source/Patches/TesterOverlayPatches.cs
@@ -13,4 +13,12 @@ public class TesterOverlayPatches
 	{
 		return Debug.isDebugBuild || SemiFunc.DebugDev();
 	}
+	
+	[HarmonyPatch(typeof(GameManager), "Start")]
+	[HarmonyPostfix]
+	[HarmonyWrapSafe]
+	private static void GameManager_Start(GameManager __instance)
+	{
+		__instance.maxPlayers = PluginLoader.maxPlayerCount.Value > 0 ? PluginLoader.maxPlayerCount.Value : GameManager.maxPlayersDefault;
+	}
 }
