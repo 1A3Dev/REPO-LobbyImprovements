9c9204b41fb3a2e8970a020050cbfb36e95e9034
1A3Dev <mcplayer1a3@gmail.com>
2026-06-04 02:13:37 +0100

    Fixed stuck in loading not activating on level loading animation

--- source/Patches/FastStartup.cs
+++ source/Patches/FastStartup.cs
@@ -1,6 +1,6 @@
-﻿using System;
-using HarmonyLib;
-using UnityEngine.SceneManagement;
+﻿using HarmonyLib;
+using Photon.Pun;
+using UnityEngine;
 
 namespace LobbyImprovements.Patches
 {
@@ -25,5 +25,42 @@ namespace LobbyImprovements.Patches
             if(PluginLoader.splashScreenUIEnabled.Value) return;
             forceSkip = true;
         }
+
+        #region Level Loading Stuck Fix
+        [HarmonyPatch(typeof(NetworkManager), nameof(NetworkManager.Update))]
+        [HarmonyPrefix]
+        [HarmonyWrapSafe]
+        private static bool NetworkManager_Update(NetworkManager __instance)
+        {
+            if(GameDirector.instance.currentState != GameDirector.gameState.Start || LoadingUI.instance.levelAnimationCompleted || MenuPagePassword.instance){
+                return true;
+            }
+
+            if(GameManager.instance.gameMode == 1 && PhotonNetwork.IsMasterClient && !__instance.LoadingDone && __instance.instantiatedPlayerAvatars >= PhotonNetwork.CurrentRoom.PlayerCount){
+                __instance.photonView.RPC("AllPlayerSpawnedRPC", RpcTarget.AllBuffered);
+                __instance.LoadingDone = true;
+            }
+
+            __instance.loadingScreenTimer += Time.deltaTime;
+            if(__instance.loadingScreenTimer >= 25f){
+                LoadingUI.instance.stuckActive = true;
+                if(SemiFunc.InputDown(InputKey.Menu)){
+                    __instance.TriggerLeavePhotonRoomForcedStuckInLoading();
+                }
+            }
+
+            return false;
+        }
+
+        [HarmonyPatch(typeof(GameDirector), nameof(GameDirector.SetStart))]
+        [HarmonyPostfix]
+        [HarmonyWrapSafe]
+        private static void GameDirector_SetStart()
+        {
+            if(NetworkManager.instance && LoadingUI.instance){
+                NetworkManager.instance.loadingScreenTimer = 10f; // 25-10 = 15s
+            }
+        }
+        #endregion
     }
 }
